1f167fb011fb0e1deaa79e1ffdcfac238c85c02e
Juan Diego Letona <juanletona@ufm.edu>
2024-11-23 23:53:59 -0600

    2.07

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Form from './pages/Form';
 import PastEntries from './pages/PastEntries';
@@ -7,21 +7,15 @@ import Stats from './pages/Stats';
 
 function App() {
   return (
-    <Router basename="/Aprendizaje">
+    <Router basename="/Aprendizaje"> {/* This sets the correct basename */}
       <Routes>
-        {/* Redirect the root route (/) to the welcome screen */}
+        {/* Redirect root (/) to the welcome screen */}
         <Route path="/" element={<Navigate to="/welcome" replace />} />
 
-        {/* Route for the welcome screen */}
+        {/* Define routes for your app */}
         <Route path="/welcome" element={<Welcome />} />
-
-        {/* Route for the daily form */}
         <Route path="/form" element={<Form />} />
-
-        {/* Route for past entries */}
         <Route path="/entries" element={<PastEntries />} />
-
-        {/* Route for statistics */}
         <Route path="/stats" element={<Stats />} />
       </Routes>
     </Router>
